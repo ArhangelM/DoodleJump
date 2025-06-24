9ec04a1d4504b216c0b6fe614ee0355e976289f6
ArhangelM <prog.cpp.cs@gmail.com>
2025-06-24 11:47:56 +0300

    Fixed UI

---
+++ Assets/DoodleJump/UI/View/SelectCameraOnCanvas.cs
@@ -0,0 +1,48 @@
+﻿using Assets.DoodleJump.Scripts.Common.SignalBus.Signals.UI;
+using Tools.Extensions;
+using Tools.SignalBus;
+using UnityEngine;
+
+namespace Assets.DoodleJump.UI.View
+{
+    [RequireComponent(typeof(Canvas))]
+    internal class SelectCameraOnCanvas : MonoBehaviour
+    {
+        private Camera _camera;
+        private Canvas _canvas;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        private void SelectCamera(StartGameSignal signal)
+        {
+            if (!_camera.HasValue())
+            {
+                _camera = Camera.main;
+                _canvas.worldCamera = _camera;
+            }
+        }
+
+        private void SubscribeEvents()
+        {
+            SignalBus.Instance.Subscribe<StartGameSignal>(SelectCamera);
+        }
+
+        private void UnsubscribeEvents()
+        {
+            SignalBus.Instance.Unsubscribe<StartGameSignal>(SelectCamera);
+        }
+    }
+}
